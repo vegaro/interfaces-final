38b4bc68f19b74bb7ddac67591a0e4a15c5c0352
Victor Espinel <lopol91@gmail.com>
2013-12-10 00:54:58 +0100

    Anadida funcionalidad butacas

--- js/compra.js
+++ js/compra.js
@@ -10,3 +10,31 @@ $('.datepicker').datepicker({
 $('select#noentradas').on('change',function(){
     $('#precio-total').text((parseFloat($('#precio').html())*this.value).toFixed(2));
 });
+
+$('select#fila').on('change',function(){
+    $('#butacas').show();
+    if(parseInt(this.value)%3==0){
+        var asientosDes=$('#asiento').children('option');
+        asientosDes.prop('disabled', false);
+        asientosDes.eq(0).prop('disabled', true);
+        asientosDes.eq(1).prop('disabled', true);
+        asientosDes.eq(3).prop('disabled', true);
+        asientosDes.eq(6).prop('disabled', true);
+    }else if(parseInt(this.value)%3==1){
+        var asientosDes=$('#asiento').children('option');
+        asientosDes.prop('disabled', false);
+        asientosDes.eq(0).prop('disabled', true);
+        asientosDes.eq(5).prop('disabled', true);
+        asientosDes.eq(6).prop('disabled', true);
+        asientosDes.eq(7).prop('disabled', true);
+    }else{
+        var asientosDes=$('#asiento').children('option');
+        asientosDes.prop('disabled', false);
+        asientosDes.eq(0).prop('disabled', true);
+        asientosDes.eq(1).prop('disabled', true);
+        asientosDes.eq(2).prop('disabled', true);
+        asientosDes.eq(3).prop('disabled', true);
+        asientosDes.eq(4).prop('disabled', true);
+    }
+
+});

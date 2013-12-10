1336657c12179f93192351e0ad9dc74d7f84a278
Cesar de la Vega <cesarvegaro@gmail.com>
2013-12-10 01:28:01 +0100

    Arregla la cruz de packery

--- js/cines.js
+++ js/cines.js
@@ -1,33 +1,43 @@
 $( document ).ready( function() {
-    var container = document.querySelector('.packery');
-    var pckry = new Packery( container );
-
-    eventie.bind( container, 'click', function() {
-        // don't proceed if item was not clicked on
-        var target = event.target;
-        if ( !classie.has( target, 'item' ) ) {
-            return;
-        }
-        var isGigante = classie.has( target, 'gigante' );
-        classie.toggleClass( target, 'gigante' );
-        $(target).children().toggle();
-        if ( isGigante ) {
-            // if shrinking, just layout
-            pckry.layout();
-        } else {
-            // if expanding, fit it
-            pckry.fit( target );
-        }
-    });
-
-    if ($.cookie('usuario')){
-        // El usuario esta logeado
-        $('#registro-login').hide();
+  var container = document.querySelector('.packery');
+  var pckry = new Packery( container );
+  
+  eventie.bind( container, 'click', function() {
+    // don't proceed if item was not clicked on
+    var target = event.target;
+    if ( !classie.has( target, 'item' ) ) {
+      return;
     }
+    var isGigante = classie.has( target, 'gigante' );
+    classie.toggleClass( target, 'gigante' );
+    $(target).children().toggle();
+    if ( isGigante ) {
+      // if shrinking, just layout
+      pckry.layout();
 
+
+    } else {
+      // if expanding, fit it
+      pckry.fit( target );
+    }
+  });
+  eventie.bind( container, 'click', function() {
+    // don't proceed if item was not clicked on
+    var target = event.target;
+    if ( !classie.has( target, 'flecha-cerrar' ) ) {
+      return;
+    }
+    console.log( $(target).parent());
+    console.log( $(target));
+    $(target).parent().toggleClass('gigante');
+    $(target).siblings().toggle();
+    $(target).toggle();
+    pckry.layout();
+  });
 });
+
 $('form').submit(function(e) {
-    var autor = $('#nombreForm').val();val()
+    var autor = $('#nombreForm').val();val();
     var comentario = $('#comentarioForm').val();
 
     // Generar el codigo html del nuevo elemento
--- js/index.js
+++ js/index.js
@@ -1,22 +1,37 @@
 $( document ).ready( function() {
-    var container = document.querySelector('.packery');
-    var pckry = new Packery( container );
+  var container = document.querySelector('.packery');
+  var pckry = new Packery( container );
+  
+  eventie.bind( container, 'click', function() {
+    // don't proceed if item was not clicked on
+    var target = event.target;
+    if ( !classie.has( target, 'item' ) ) {
+      return;
+    }
+    var isGigante = classie.has( target, 'gigante' );
+    classie.toggleClass( target, 'gigante' );
+    $(target).children().toggle();
+    if ( isGigante ) {
+      // if shrinking, just layout
+      pckry.layout();
 
-    eventie.bind( container, 'click', function() {
-        // don't proceed if item was not clicked on
-        var target = event.target;
-        if ( !classie.has( target, 'item' ) ) {
-            return;
-        }
-        var isGigante = classie.has( target, 'gigante' );
-        classie.toggleClass( target, 'gigante' );
-        $(target).children().toggle();
-        if ( isGigante ) {
-            // if shrinking, just layout
-            pckry.layout();
-        } else {
-            // if expanding, fit it
-            pckry.fit( target );
-        }
-    });
+
+    } else {
+      // if expanding, fit it
+      pckry.fit( target );
+    }
+  });
+  eventie.bind( container, 'click', function() {
+    // don't proceed if item was not clicked on
+    var target = event.target;
+    if ( !classie.has( target, 'flecha-cerrar' ) ) {
+      return;
+    }
+    console.log( $(target).parent());
+    console.log( $(target));
+    $(target).parent().toggleClass('gigante');
+    $(target).siblings().toggle();
+    $(target).toggle();
+    pckry.layout();
+  });
 });

e0a063ba81e37a3cd66a5149d1baa55491baccae
Victor Espinel <lopol91@gmail.com>
2013-12-10 15:45:07 +0100

    anadiddo datepicker a pagina de Registro.html

--- js/registro.js
+++ js/registro.js
@@ -20,3 +20,12 @@ $('#form-registro').submit(function(e){
     }
     event.preventDefault();
 });
+
+var nowTemp = new Date();
+var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
+
+$('.datepicker').datepicker({
+    onRender: function(date) {
+    return date.valueOf() > now.valueOf() ? 'disabled' : '';
+  }
+});

16cc2d03726aebd8e6ec88c2d8a6435573d5f7ac
Cesar de la Vega <cesarvegaro@gmail.com>
2013-12-10 03:36:54 +0100

    Borra la carpeta script y empieza a arreglar la pelicula

---
+++ js/peli.js
@@ -0,0 +1,10 @@
+$(function () {
+    $("#slider3").responsiveSlides({
+        manualControls: '#slider3-pager',
+        maxwidth: 540
+    });
+});
+
+$(document).ready(function(){
+    $(".youtube").colorbox({iframe:true, innerWidth:640, innerHeight:390});
+});
--- script/script.js
+++
@@ -1,4 +0,0 @@
-
-        $(function() {
-            $('div.gallery img').slidingGallery();
-        });
